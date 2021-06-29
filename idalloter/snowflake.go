30e4fa1586f72eec50d24b1e403bd503a9b8d751
xiyouhpy <xiyouhanpeiyan@gmail.com>
2021-06-29 15:57:13 +0800

    add idalloter for snow flake

--- idalloter/snowflake.go
+++ idalloter/snowflake.go
@@ -1,7 +1,11 @@
 // Package idalloter 雪花算法
 // +----------------------------------------------------------------------+
-// | 1 bit ｜ 41 Bit lastTimestamp | 10 Bit machineID | 12 Bit sequenceID |
+// | 1 bit ｜ 41 Bit lastStamp | 10 Bit machineID | 12 Bit sequenceID |
 // +----------------------------------------------------------------------+
+// 1  bit，这里最高位为标识位预留出来，默认为0
+// 41 bit，毫秒级时间戳，存储时间戳差值（当前时间戳-开始时间戳）；开始时间戳可任意指定，一般为系统上线时间，使用该库时建议调整，最多可使用 69 年
+// 10 bit，机器标识，支持最多部署 1024 个节点
+// 12 bit，计数序列号，序列号为一系列自增 id，支持每个节点每毫秒最多产生 4096 个序号
 package idalloter
 
 import (
@@ -16,15 +20,18 @@ const maxSequenceID = 1<<12 - 1
 // maxMachineID 为机器标识号，占 10 bit，最大值为 2^10-1
 const maxMachineID = 1<<10 - 1
 
+// startStamp 开始时间戳，示例为：2021-06-01 00:00:00 000
+const startStamp uint64 = 1622476800000
+
 // SnowFlake 雪花算法结构体，64字节长度的 id 共由三部分组成
-//           lastTimestamp 42bit    上一次时间戳，单位毫秒
+//           lastStamp     42bit    上一次时间戳，单位毫秒
 //           sequenceID    12bit    序列号，使同一毫秒内不出现重复
 //           machineID     10bit    机器标识id，使分布式系统内不同机器间不出现重复
 type SnowFlake struct {
-	lastTimestamp uint64
-	sequenceID    uint32
-	machineID     uint32
-	lock          sync.Mutex
+	lastStamp  uint64
+	sequenceID uint32
+	machineID  uint32
+	lock       sync.Mutex
 }
 
 // SnowFlakeInterface 接口整理
@@ -46,24 +53,24 @@ func NewSnowFlake(machineId uint32) (*SnowFlake, error) {
 
 // getNextMillisecond 获取下一个毫秒的时间戳
 func (sf *SnowFlake) getNextMillisecond(ntime uint64) uint64 {
-	for ntime == sf.lastTimestamp {
+	for ntime == sf.lastStamp {
 		time.Sleep(time.Microsecond * 100)
 	}
 
-	return getTimestamp()
+	return getStamp()
 }
 
-// getTimestamp 获取当前时间戳，单位毫秒
-func getTimestamp() uint64 {
-	return uint64(time.Now().UnixNano() / int64(time.Millisecond))
+// getStamp 获取当前时间戳差值
+func getStamp() uint64 {
+	return uint64(time.Now().UnixNano()/int64(time.Millisecond)) - startStamp
 }
 
 // getId 根据记录的结构，得到唯一id
 func (sf *SnowFlake) getId() uint64 {
-	// 时间戳在高位，先将 lastTimestamp 向左移动 10 + 12 个bit，预留出 machineID 和 sequenceID 的位置
+	// 时间戳在高位，先将 lastStamp 向左移动 10 + 12 个bit，预留出 machineID 和 sequenceID 的位置
 	// 再将 machineID 向左移动 10 个bit，为 sequenceID 预留这 10 个bit
 	// 最后将 sequenceID 填补到这 10 个bit
-	return sf.lastTimestamp<<(10+12) | uint64(sf.machineID)<<10 | uint64(sf.sequenceID)
+	return sf.lastStamp<<(10+12) | uint64(sf.machineID)<<10 | uint64(sf.sequenceID)
 }
 
 // GenerateID 获取唯一id
@@ -71,24 +78,18 @@ func (sf *SnowFlake) GenerateID() (uint64, error) {
 	sf.lock.Lock()
 	defer sf.lock.Unlock()
 
-	// 判断当前系统得到的时间戳是否小于记录的最小时间戳，判断系统是否异常
-	nowTime := getTimestamp()
-	if nowTime < sf.lastTimestamp {
+	nowTime := getStamp()
+	if nowTime < sf.lastStamp {
 		return 0, errors.New("invalid system time")
-	}
-
-	// 若系统时间戳和记录的最后时间戳相等，说明在该毫秒内产生了碰撞，利用 sequenceID 解决碰撞
-	if nowTime == sf.lastTimestamp {
+	} else if nowTime > sf.lastStamp {
+		sf.sequenceID = 0
+	} else {
 		sf.sequenceID = (sf.sequenceID + 1) & maxSequenceID
 		if sf.sequenceID == 0 {
 			nowTime = sf.getNextMillisecond(nowTime)
 		}
-	} else {
-		sf.sequenceID = 0
 	}
-
-	// 记录本次分配 id 的时间戳
-	sf.lastTimestamp = nowTime
+	sf.lastStamp = nowTime
 
 	return sf.getId(), nil
 }
