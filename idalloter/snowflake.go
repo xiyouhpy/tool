4cf6b5fa72801a890ee3b5eb86b3362949219fab
xiyouhpy <xiyouhanpeiyan@gmail.com>
2021-07-07 11:45:59 +0800

    fix snowflake

--- idalloter/snowflake.go
+++ idalloter/snowflake.go
@@ -1,7 +1,7 @@
-// Package idalloter 雪花算法
-// +----------------------------------------------------------------------+
-// | 1 bit ｜ 41 Bit lastStamp | 10 Bit machineID | 12 Bit sequenceID |
-// +----------------------------------------------------------------------+
+// Package idalloter 雪花算法，按照时间有序生成唯一id
+// +--------------------------------------------------------------------------+
+// |  1 bit  ｜  41 Bit lastStamp  |  10 Bit machineID  |  12 Bit sequenceID  ｜
+// +--------------------------------------------------------------------------+
 // 1  bit，这里最高位为标识位预留出来，默认为0
 // 41 bit，毫秒级时间戳，存储时间戳差值（当前时间戳-开始时间戳）；开始时间戳可任意指定，一般为系统上线时间，使用该库时建议调整，最多可使用 69 年
 // 10 bit，机器标识，支持最多部署 1024 个节点
@@ -16,22 +16,23 @@ import (
 
 const (
 	lastStampBit = 41 // 上一次时间戳占用 bit
+	workBit      = 10 // 机器识别号占用 bit
 	sequenceBit  = 12 // 序列号占用 bit
-	machineBit   = 10 // 机器识别号占用 bit
 
-	maxSequenceID        = 1<<sequenceBit - 1 // 序列号，12 bit，最大值为 2^12-1
-	maxMachineID         = 1<<machineBit - 1  // 机器标识号，10 bit，最大值为 2^10-1
-	startStamp    uint64 = 1622476800000      // 开始时间戳，2021-06-01 00:00:00 000
+	maxWorkID     = 1<<workBit - 1     // 机器标识号，10 bit，最大值为 2^10-1
+	maxSequenceID = 1<<sequenceBit - 1 // 序列号，12 bit，最大值为 2^12-1
+
+	startStamp uint64 = 1622476800000 // 开始时间戳，2021-06-01 00:00:00 000
 )
 
 // SnowFlake 雪花算法结构体，64字节长度的 id 共由三部分组成
 //           lastStamp     42bit    上一次时间戳，单位毫秒
 //           sequenceID    12bit    序列号，使同一毫秒内不出现重复
-//           machineID     10bit    机器标识id，使分布式系统内不同机器间不出现重复
+//           workID        10bit    机器标识id，使分布式系统内不同机器间不出现重复
 type SnowFlake struct {
 	lastStamp  uint64
 	sequenceID uint32
-	machineID  uint32
+	workID     uint32
 	lock       sync.Mutex
 }
 
@@ -44,12 +45,12 @@ type SnowFlakeInterface interface {
 }
 
 // NewSnowFlake 获取 id 分配器对象
-func NewSnowFlake(machineId uint32) (*SnowFlake, error) {
-	if machineId < 0 || machineId > maxMachineID {
+func NewSnowFlake(workId uint32) (*SnowFlake, error) {
+	if workId < 0 || workId > maxWorkID {
 		return nil, errors.New("invalid machineId")
 	}
 
-	return &SnowFlake{machineID: machineId}, nil
+	return &SnowFlake{workID: workId}, nil
 }
 
 // getNextMillisecond 获取下一个毫秒的时间戳
@@ -68,7 +69,7 @@ func getStamp() uint64 {
 
 // getId 根据记录的结构，得到唯一id
 func (sf *SnowFlake) getId() uint64 {
-	return sf.lastStamp<<(machineBit+sequenceBit) | uint64(sf.machineID)<<machineBit | uint64(sf.sequenceID)
+	return sf.lastStamp<<(workBit+sequenceBit) | uint64(sf.workID)<<sequenceBit | uint64(sf.sequenceID)
 }
 
 // GenerateID 获取唯一id
